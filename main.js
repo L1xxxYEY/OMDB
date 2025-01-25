e17cd9f9159cef751e029621f67a22a31010472c
L1xxxYEY <sergeevamarinaaaa2004@gmail.com>
2025-01-25 21:19:09 +0500

    ready

--- main.js
+++ main.js
@@ -1,37 +1,93 @@
 //Элементы
 const body = document.querySelector("body");
+const search = document.getElementById("search");
+const main = document.querySelector(".main");
+const movieTitle = document.querySelectorAll(".movieTitle");
+const movie = document.querySelector(".movie");
+const movieImg = document.querySelector(".movieImg");
+const movieDesc = document.querySelector(".movieDescription");
+const similarMovie = document.querySelector(".similarMovie");
+const similarCard = document.querySelectorAll(".similarCard");
 
 //Кнопки
 const themeBtn = document.querySelector("#themeChange");
-
+const searchBtn = document.querySelector("#searchBtn");
 
 //Слушатели событий
-themeBtn.addEventListener("click",themeChange);
+themeBtn.addEventListener("click", themeChange);
+searchBtn.addEventListener("click", findMovie);
 
+function themeChange() {
+  body.classList.toggle("dark");
+}
 
-function themeChange(){
-body.classList.toggle("dark")
+async function findMovie() {
+  const loader = document.querySelector(".loader");
+  loader.style.display = "block";
+
+  const data = { apikey: "1b7ff984", t: search.value };
+  const response = await sendRequest("https://www.omdbapi.com", "GET", data);
+  loader.style.display = "none";
+
+  if (response.Response === "False") {
+    main.style.display = "block";
+    movie.style.display = "none";
+    movieTitle[0].style.display = "block";
+    movieTitle[0].innerHTML = "Введите название фильма"
+  } else {
+    showMovie(response);
+  }
+}
+
+function showMovie(data) {
+  main.style.display = "block";
+  movieTitle[0].style.display = "block";
+  movie.style.display = "flex";
+  movieImg.style.backgroundImage = `url(${data.Poster})`;
+  movieTitle[0].innerHTML = data.Title;
+
+  let params = [
+    "imdbRating",
+    "Year",
+    "Released",
+    "Genre",
+    "Country",
+    "Language",
+    "Director",
+    "Writer",
+    "Actors",
+  ];
+
+  movieDesc.innerHTML = ""
+  params.forEach((elem)=>{
+    movieDesc.innerHTML += `
+                <div class="desc">
+                  <p>${elem}</p>
+                  <p>${data[elem]}</p>
+                </div>
+                `;
+  })
 }
 
 async function sendRequest(url, method, data) {
-    if (method == "POST") {
-      let response = await fetch(url, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+  if (method == "POST") {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-      response = await response.json();
-      return response;
-    } else if (method == "GET") {
-      url = url + "?" + new URLSearchParams(data);
-      let response = await fetch(url, {
-        method: "GET",
-      });
-      response = await response.json();
-      return response;
-    }
+    response = await response.json();
+    return response;
+  } else if (method == "GET") {
+    url = url + "?" + new URLSearchParams(data);
+    let response = await fetch(url, {
+      method: "GET",
+    });
+    response = await response.json();
+    return response;
   }
+}
